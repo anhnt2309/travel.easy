37c0c16ae2d549fa5842e079a5f2b71aeac10619
Tuan_Anh <anhnt.2309@gmail.com>
2017-05-16 15:33:07 +0700

    first commit

---
+++ js/datetimepicker.js
@@ -0,0 +1,5 @@
+  var options = {};
+  $(document).ready(function(){
+  		alert('abc');
+  	  $('#one').datepicker(options);
+  });
---
+++ php/airport-autocomplete.php
@@ -0,0 +1,40 @@
+<?php
+
+$from = $_GET['from-location'];
+
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=6NwaGnAUxDUPV2MEFhAW0cR9uhGAQ4ol&term=".$from,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_FOLLOWLOCATION=> TRUE,
+	CURLOPT_SSL_VERIFYPEER=> false,
+	));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	// echo $response;
+	if($response != null){
+		$response_array = json_decode($response);
+
+		foreach ($response_array as $key => $value) {
+			$first = strtok($value->label, '[');
+			echo $value->value ." -- ". $first ."<br>";
+		}
+	}
+}
+
+?>
