dc8f224a686e4e1e1b3d6852016a38a0c0bfe8ea
Tuan_Anh <anhnt.2309@gmail.com>
2017-05-18 16:58:57 +0700

    zzz

--- js/pikaday_custom.js
+++ js/pikaday_custom.js
@@ -1,13 +1,13 @@
 var start_date = new Pikaday({
-	format: 'DD / MM / YYYY',
-    field: document.getElementByName('checkin'),
+    format: 'DD / MM / YYYY',
+    field: document.getElementById('start_date'),
     onSelect: function() {
         end_date.setMinDate(this.getDate());
     }
 });
 var end_date = new Pikaday({
-	format: 'DD / MM / YYYY',
-    field: document.getElementByName('checkout'),
+    format: 'DD / MM / YYYY',
+    field: document.getElementById('end_date'),
     onSelect: function() {
         start_date.setMaxDate(this.getDate());
     }
@@ -18,12 +18,12 @@ jQuery(document).on('change', '.pika-select-month', function() {
 });
 
 jQuery(document).on('change', '.pika-select-year', function() {
-	pika_modif_date('year', this.value);
+    pika_modif_date('year', this.value);
 });
 
 
 function pika_modif_date(type, value_change){
-	var pik_obj;
+    var pik_obj;
     // identifying object
     if( start_date.isVisible() ){
         pik_obj = 'start_date';
@@ -40,12 +40,12 @@ function pika_modif_date(type, value_change){
     }
     // Modify month or yeah
     if( 'month' == type ){
-		actual_date.setMonth(value_change);
+        actual_date.setMonth(value_change);
     }
-	else{
-		actual_date.setFullYear(value_change);
-	}
-	// Format for set to pikaday
-	year = 1900 + actual_date.getYear();
+    else{
+        actual_date.setFullYear(value_change);
+    }
+    // Format for set to pikaday
+    year = 1900 + actual_date.getYear();
     eval(pik_obj+'.setDate(new Date(' + year +','+ actual_date.getMonth() +','+ actual_date.getDate() +'), 1);');
 }
